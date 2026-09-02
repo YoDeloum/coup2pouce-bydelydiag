f028bde7dc37acbfbc7316ee6004fdfb28e9471a
YoDeloum <y.deloum@delydiag.fr>
2026-09-02 23:05:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Coup 2 Pouce DELY DIAG
-const CACHE = 'coup2pouce-v14';
+const CACHE = 'coup2pouce-v15';
 
 self.addEventListener('install', function(e) {
   // Ne pas skipWaiting automatiquement : on attend que l'utilisateur confirme la mise à jour
